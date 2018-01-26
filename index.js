f183696a713dbd7f9c50f1748ff4016169c7ef60
Ranvir Gorai <gorairanvir@gmail.com>
2018-01-26 15:25:18 +0530

    logout added

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import { AsyncStorage } from 'react-native';
-import { init, login, isLogin, register } from './lib/api';
-
+import { init, login, isLogin, register,getUser } from './lib/api';
 const UNKNOWN_ERROR = 'There was an unknown error.';
 
 export default {
     init: (API_KEY, BASE_URL) => init(API_KEY, BASE_URL),
     login: (data) => login(data),
-    isLogin: () => isLogin(),
-    register: (data) => register(data)
+    register: (data) => register(data),
+    isLogin,
+    getUser
 }
+
